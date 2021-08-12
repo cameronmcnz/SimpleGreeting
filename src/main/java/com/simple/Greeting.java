b7e7a7e956172a42ec6838a1b936458ccf6f79d9
learn-devops-fast <41976996+learn-devops-fast@users.noreply.github.com>
2021-08-12 10:32:44 -0400

    Update Greeting.java

--- src/main/java/com/simple/Greeting.java
+++ src/main/java/com/simple/Greeting.java
@@ -1,23 +1,12 @@
 package com.simple;
 
-
-
-/**
- * Generate a unique number
- *
- */
-public class Greeting 
-{
-
-    public static void main( String[] args )
-    {
-        Greeting msg = new Greeting();
+public class Greeting {
+    public static void main( String[] args ){
+       Greeting msg = new Greeting();
        System.out.println(msg.getStatus());
     }
     
     public String getStatus(){
-    	
     	return "GOOD";
-    	
     }
 }
